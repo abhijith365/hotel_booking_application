f3c0702f0818f23dfade37c5fa185dcfb7731a97
abijith365 <abhijithelampal005@gmail.com>
2022-07-31 23:30:11 +0530

    room router v1

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import { allRoom, createRoom, deleteRoom, singlRoom, updateRoom } from '../controllers/Room.js'
+import { adminVerify } from '../utils/verfiToken.js'
 const route = express.Router()
 
-route.get('/', (req, res) => { res.send("coming from auth") })
+// create room
+route.post('/:hotelId', adminVerify, createRoom)
+
+// update room
+route.put('/update/:roomId', adminVerify, updateRoom)
+
+//delete room 
+route.delete('/delete/:id/:hotelId', adminVerify, deleteRoom)
+
+//get single room 
+route.get('/:roomId', singlRoom)
+
+//get all rooms
+route.get('/', allRoom)
 
 export default route
