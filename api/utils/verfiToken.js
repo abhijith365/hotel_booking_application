45005e33da6ade23e124c4ceba42afe9e070b3aa
abijith365 <abhijithelampal005@gmail.com>
2022-07-31 14:31:11 +0530

    added admin verification, user verification

--- api/utils/verfiToken.js
+++ api/utils/verfiToken.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { createError } from './error.js'
 
-export const verfiToken = (req, res, next) => {
+export const verifiToken = (req, res, next) => {
+
     const token = req.cookies.access_token
-    if (!token) {
-        return next(createError(401, "Your are not authenticated"))
-    }
+
+    if (!token) return next(createError(401, "Your are not authenticated"))
+
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
 
@@ -14,5 +15,37 @@ export const verfiToken = (req, res, next) => {
         req.user = user;
 
         next()
+    })
+}
+
+// cheking is user
+export const userVerfi = (req, res, next) => {
+
+    verifiToken(req, res, () => {
+        const userId = (req.user) ? (req.user.id) ? req.user.id : null : null
+        const isAdmin = (req.user) ? (req.user.isAdmin) ? req.user.isAdmin : null : null
+
+        if (req.params.id === userId || isAdmin) {
+            next()
+        } else {
+            return next(createError(403, "Your not authorized"))
+        }
+
+    })
+}
+
+export const adminVerify = (req, res, next) => {
+    verifiToken(req, res, () => {
+        try {
+            const isAdmin = (req.user) ? (req.user.isAdmin) ? req.user.isAdmin : null : null
+            if (isAdmin) {
+                next()
+            } else {
+                next(createError(403, "Your not authorized"))
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
     })
 }
