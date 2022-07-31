e4dffbfd8fddfd02a7cb6563414ea8878a70afbb
abijith365 <abhijithelampal005@gmail.com>
2022-07-31 12:46:07 +0530

    added custom error handler

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,1 +1,9 @@
-export const createError = () => { }
+//CUSTOM ERROR HANDLER 
+export const createError = (status, message) => {
+
+    const err = new Error()
+    err.status = status || 500
+    err.message = message || "Something went wrong"
+
+    return err;
+}
